d93cba29302e1ac50c82c060109feb8332d48e17
balibhai1010 <siddhant22496@iiitd.ac.in>
2024-01-07 05:13:39 +0530

    Scripts

---
+++ run_game.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Compile the source code
+gcc Tic_Tac_Toe_game.c -o tic_tac_toe
+
+REM Run the compiled executable
+tic_tac_toe.exe
---
+++ run_game.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Compile the source code
+gcc Tic_Tac_Toe_game.c -o tic_tac_toe
+
+# Run the compiled executable
+./tic_tac_toe
